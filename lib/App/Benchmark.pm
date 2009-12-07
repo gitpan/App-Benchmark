068e23ba85e5788688c6137bafe566903c250ceb
Marcel Grünauer <marcel@cpan.org>
2009-12-07 21:49:29 +0000

    Import of MARCEL/App-Benchmark-0.04 from CPAN.
    gitpan-cpan-distribution: App-Benchmark
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MARCEL/App-Benchmark-0.04.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/App/Benchmark.pm
+++ lib/App/Benchmark.pm
@@ -1,48 +1,33 @@
 package App::Benchmark;
-
+use 5.008;
 use strict;
 use warnings;
 use Test::More;
 use Benchmark qw(cmpthese timethese :hireswallclock);
 use IO::Capture::Stdout;
-
-
-our $VERSION = '0.03';
-
-
+our $VERSION = '0.04';
 use base 'Exporter';
-
-
 our @EXPORT = ('benchmark_diag');
-
 
 sub benchmark_diag {
     my ($iterations, $benchmark_hash) = @_;
     my $capture = IO::Capture::Stdout->new;
-
     $capture->start;
     cmpthese(timethese($iterations, $benchmark_hash));
     $capture->stop;
-
     {
         my $previous_default = select(STDOUT);
-        $|++; # autoflush STDOUT
+        $|++;    # autoflush STDOUT
         select(STDERR);
-        $|++; # autoflush STDERR
+        $|++;    # autoflush STDERR
         select($previous_default);
     }
-
     chomp(my @lines = $capture->read);
     warn "# $_\n" for @lines;
-
     plan tests => 1;
     pass('benchmark');
 }
-
-
 1;
-
-
 __END__
 
 =head1 NAME
@@ -73,12 +58,12 @@ Ricardo Signes came up with the idea.
 
 =over 4
 
-=item benchmark_diag
+=item C<benchmark_diag>
 
 Takes a number of iterations and a benchmark definition hash, just like
 C<timethese()> from the L<Benchmark> module. Runs the benchmarks and reports
 them, each line prefixed by a hash sign so it doesn't mess up the TAP output.
-Also, a pseudotest is being generated to keep the testing framework happy.
+Also, a dummy test is being generated to keep the testing framework happy.
 
 This function is exported automatically.
 
@@ -99,7 +84,7 @@ See perlmodinstall for information and options on installing Perl modules.
 
 The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+site near you. Or see L<http://search.cpan.org/dist/App-Benchmark/>.
 
 =head1 AUTHORS
 
@@ -107,7 +92,7 @@ Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008 by Marcel GrE<uuml>nauer
+Copyright 2008-2009 by Marcel GrE<uuml>nauer
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
